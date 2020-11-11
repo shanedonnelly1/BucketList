ef19e527f15f0a3aabd92eb1c1efad0a29674487
Shane Donnelly <shanedonnelly1@hotmail.com>
2020-11-11 09:09:55 +0000

    Added up to day 2 of project.

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,14 +5,112 @@
 //  Created by Shane on 10/11/2020.
 //
 
+import LocalAuthentication
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isUnlocked = false
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            if self.isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, _ in
+                DispatchQueue.main.async {
+                    if success {
+                        self.isUnlocked = true
+                    } else {
+                        // there was a problem
+                    }
+                }
+            }
+        } else {
+            // no biometrics
+        }
     }
 }
+
+//struct ContentView: View {
+//    var body: some View {
+//        MapView()
+//            .edgesIgnoringSafeArea(.all)
+//    }
+//}
+
+//struct LoadingView: View {
+//    var body: some View {
+//        Text("Loading...")
+//    }
+//}
+//
+//struct SuccessView: View {
+//    var body: some View {
+//        Text("Success!")
+//    }
+//}
+//
+//struct FailedView: View {
+//    var body: some View {
+//        Text("Failed.")
+//    }
+//}
+//
+//struct ContentView: View {
+//    enum LoadingState {
+//        case loading, success, failed
+//    }
+//    @State private var loadingState = LoadingState.loading
+//
+//    var body: some View {
+//        if loadingState == .loading {
+//            LoadingView()
+//        } else if loadingState == .success {
+//            SuccessView()
+//        } else {
+//            FailedView()
+//        }
+//    }
+//}
+
+//extension FileManager {
+//    static func getDocumentDirectory() -> URL {
+//        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+//        return paths[0]
+//    }
+//}
+//
+//struct ContentView: View {
+//
+//    var body: some View {
+//        Text("Hello, world!")
+//            .onTapGesture {
+//                let str = "Test Message"
+//                let url = FileManager.getDocumentDirectory().appendingPathComponent("message.txt")
+//
+//                do {
+//                    try str.write(to: url, atomically: true, encoding: .utf8)
+//                    let input = try String(contentsOf: url)
+//                    print(input)
+//                } catch {
+//                    print(error.localizedDescription)
+//                }
+//            }
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
